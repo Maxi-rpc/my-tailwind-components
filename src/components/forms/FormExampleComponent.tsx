2690f093a8141cb689c8e1f129166f5d12623df5
Maximiliano <maxirpc2607@gmail.com>
2024-08-23 22:38:21 -0300

    update text input and type

--- src/components/forms/FormExampleComponent.tsx
+++ src/components/forms/FormExampleComponent.tsx
@@ -52,57 +52,31 @@ export const FormExampleComponent = ({
 
 									<div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 										<div className="sm:col-span-3">
-											<label
-												htmlFor="first-name"
-												className="block text-sm font-medium leading-6 text-gray-900"
-											>
-												First name
-											</label>
-											<div className="mt-2">
-												<input
-													id="first-name"
-													name="first-name"
-													type="text"
-													autoComplete="given-name"
-													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-												/>
-											</div>
+											<TextInputComponent
+												label="First name"
+												id="first-name"
+												name="first-name"
+												autoComplete="given-name"
+											/>
 										</div>
 
 										<div className="sm:col-span-3">
-											<label
-												htmlFor="last-name"
-												className="block text-sm font-medium leading-6 text-gray-900"
-											>
-												Last name
-											</label>
-											<div className="mt-2">
-												<input
-													id="last-name"
-													name="last-name"
-													type="text"
-													autoComplete="family-name"
-													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-												/>
-											</div>
+											<TextInputComponent
+												label="Last name"
+												id="last-name"
+												name="last-name"
+												autoComplete="family-name"
+											/>
 										</div>
 
 										<div className="sm:col-span-4">
-											<label
-												htmlFor="email"
-												className="block text-sm font-medium leading-6 text-gray-900"
-											>
-												Email address
-											</label>
-											<div className="mt-2">
-												<input
-													id="email"
-													name="email"
-													type="email"
-													autoComplete="email"
-													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-												/>
-											</div>
+											<TextInputComponent
+												label="Email address"
+												id="email"
+												name="email"
+												autoComplete="email"
+												type="email"
+											/>
 										</div>
 
 										<div className="sm:col-span-3">
--- src/components/forms/text/TextInputComponent.tsx
+++ src/components/forms/text/TextInputComponent.tsx
@@ -2,20 +2,22 @@ interface TextInputProps {
 	children?: React.ReactNode;
 	customClass?: React.ComponentProps<"div">["className"];
 	label?: string;
-	path?: string;
 	id?: string;
 	name?: string;
 	placeHolder?: string;
+	autoComplete?: string;
+	type?: string;
 }
 
 export const TextInputComponent = ({
 	children,
 	customClass,
 	label,
-	path,
 	id,
 	name,
 	placeHolder,
+	autoComplete,
+	type = "text",
 }: TextInputProps) => {
 	return (
 		<>
@@ -29,19 +31,13 @@ export const TextInputComponent = ({
 					</label>
 				)}
 				<div className="mt-2">
-					<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-						<span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
-							{path}
-						</span>
-						<input
-							id={id}
-							name={name}
-							type="text"
-							placeholder={placeHolder}
-							autoComplete=""
-							className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-						/>
-					</div>
+					<input
+						id={id}
+						name={name}
+						type={type}
+						autoComplete={autoComplete}
+						className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+					/>
 				</div>
 			</div>
 		</>
