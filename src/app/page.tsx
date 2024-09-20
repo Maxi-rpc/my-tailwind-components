a17cd35c9c058d9b027c289f2fb0bff89142ec10
Maximiliano <maxirpc2607@gmail.com>
2024-09-20 20:14:26 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,7 @@
-"use client";
-import {
-	GridComponent,
-	NavBarComponent,
-	SpaceComponent,
-} from "@/components";
-
 export default function Home() {
 	return (
 		<main>
-			<NavBarComponent></NavBarComponent>
-			<SpaceComponent />
-			<GridComponent container>
-				<GridComponent item customClass="bg-red-400 col-span-3"></GridComponent>
-				<GridComponent
-					item
-					customClass="bg-blue-400 col-span-6"
-				></GridComponent>
-				<GridComponent
-					item
-					customClass="bg-green-400 col-span-3"
-				></GridComponent>
-			</GridComponent>
+			<h1>Home page</h1>
 		</main>
 	);
 }
