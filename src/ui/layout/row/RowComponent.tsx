cb9d6d8d722f76fe774441c69e2734c40c480976
Maximiliano <maxirpc2607@gmail.com>
2024-09-27 20:05:53 -0300

    Update RowComponent.tsx

--- src/ui/layout/row/RowComponent.tsx
+++ src/ui/layout/row/RowComponent.tsx
@@ -6,11 +6,30 @@ export const RowComponent = ({
 	itemsAlign = "center",
 	justify = "between",
 }: RowProps) => {
-	const classItemsAlign = `items-${itemsAlign}`;
-	const classJustify = `justify-${justify}`;
+	const itemVariant = {
+		start: "items-start",
+		end: "items-end",
+		center: "items-center",
+		baseline: "items-baseline",
+		stretch: "items-stretch",
+	};
+
+	const justifyVariant = {
+		normal: "justify-normal",
+		start: "justify-start",
+		end: "justify-end",
+		center: "justify-center",
+		between: "justify-between",
+		around: "justify-around",
+		evenly: "justify-evenly",
+		stretch: "justify-stretch",
+	};
+
 	return (
 		<>
-			<div className={`${customClass} flex ${classItemsAlign} ${classJustify}`}>
+			<div
+				className={`${customClass} flex ${itemVariant[itemsAlign]} ${justifyVariant[justify]}`}
+			>
 				{children}
 			</div>
 		</>
